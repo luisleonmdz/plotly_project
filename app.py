ec8e3a28f3f629bdd94d9964bbf0392def2efe3d
luisleonmdz <luisleonm_98@hotmail.com>
2025-05-06 09:59:40 -0600

    grafico de dispersion

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
      
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
+scatter_button = st.button('Construir una gráfica de dispersión')
+
+st.header ('PROYECTO SPRINT 7:')
      
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -14,3 +17,13 @@ if hist_button: # al hacer clic en el botón
      
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Gráfico de dispersión para anuncios y precio')
+         
+# crear un histograma
+    fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_2, use_container_width=True)
